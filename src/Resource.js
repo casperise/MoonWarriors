aab469b9a4a0858c2f5429e57c1f78bc5c3b792f
Jozef Prídavok <jozef@pridavok.sk>
2013-03-08 00:28:37 +0100

    The first upload.

---
+++ src/Resource.js
@@ -0,0 +1,40 @@
+var dirImg = "res/";
+var dirMusic = "res/Music/";
+
+//image
+var s_bg01 = dirImg + "bg01.jpg";
+var s_loading = dirImg + "loading.png";
+var s_ship01 = dirImg + "ship01.png";
+var s_menu = dirImg + "menu.png";
+var s_logo = dirImg + "logo.png";
+var s_cocos2dhtml5 = dirImg + "cocos2d-html5.png";
+var s_gameOver = dirImg + "gameOver.png";
+var s_menuTitle = dirImg + "menuTitle.png";
+var s_Enemy = dirImg + "Enemy.png";
+var s_flare = dirImg + "flare.jpg";
+var s_bullet = dirImg + "bullet.png";
+var s_explosion = dirImg + "explosion.png";
+var s_explode1 = dirImg + "explode1.jpg";
+var s_explode2= dirImg + "explode2.jpg";
+var s_explode3 = dirImg + "explode3.jpg";
+var s_hit = dirImg + "hit.jpg";
+var s_arial14 = dirImg + "arial-14.png";
+var s_arial14_fnt = dirImg + "arial-14.fnt";
+
+//music
+var s_bgMusic = dirMusic + "bgMusic.mp3";
+var s_mainMainMusic = dirMusic + "mainMainMusic.mp3";
+
+//effect
+var s_buttonEffect = dirMusic + "buttonEffet.mp3";
+var s_explodeEffect = dirMusic + "explodeEffect.mp3";
+var s_fireEffect = dirMusic + "fireEffect.mp3";
+var s_shipDestroyEffect = dirMusic + "shipDestroyEffect.mp3";
+
+//tmx
+var s_level01 = dirImg + "level01.tmx";
+
+//plist
+var s_Enemy_plist = dirImg + "Enemy.plist";
+var s_explosion_plist = dirImg + "explosion.plist";
+var s_bullet_plist = dirImg + "bullet.plist";
